ad18fdd2e38006e11ac757392aaf5318d951d65c
bobliu0909 <bobliu0909@gmail.com>
2018-10-13 16:26:42 +0800

    agent config params adjust, modify `DockerAgentIPAddr` to `DockerNodeHTTPAddr` and `DockerClusterPortsRange` to `DockerContainerPortsRange`.

--- models/config.go
+++ models/config.go
@@ -3,18 +3,18 @@ package models
 // Config - config info
 type Config struct {
 	AppVersion                  string `json:"AppVersion"`
-	DockerEndPoint              string `json:"DockerEndPoint`
-	DockerAPIVersion            string `json:"DockerAPIVersion`
+	DockerEndPoint              string `json:"DockerEndPoint"`
+	DockerAPIVersion            string `json:"DockerAPIVersion"`
 	DockerRegistryAddress       string `json:"-"`
 	EnableBuildImage            bool   `json:"-"`
 	DockerComposePath           string `json:"DockerComposePath"`
 	DockerComposePackageMaxSize int64  `json:"DockerComposePackageMaxSize"`
-	DockerAgentIPAddr           string `json:"DockerAgentIPAddr"`
+	DockerNodeHTTPAddr          string `json:"DockerNodeHTTPAddr"`
+	DockerContainerPortsRange   string `json:"DockerContainerPortsRange"`
 	DockerClusterEnabled        bool   `json:"DockerClusterEnabled"`
 	DockerClusterURIs           string `json:"DockerClusterURIs"`
 	DockerClusterName           string `json:"DockerClusterName"`
 	DockerClusterHeartBeat      string `json:"DockerClusterHeartBeat"`
 	DockerClusterTTL            string `json:"DockerClusterTTL"`
-	DockerClusterPortsRange     string `json:"DockerClusterPortsRange"`
 	LogLevel                    int    `json:"LogLevel"`
 }
